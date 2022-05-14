48a5ef96f56edd072cbaebb44023a1c68a8ce4cc
andrehsym <kochetkovandre1993@gmail.com>
2022-05-14 15:47:29 +0300

    Пустой проект

---
+++ src/main/java/shelterhelper/ShelterHelperApplication.java
@@ -0,0 +1,13 @@
+package shelterhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelterHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShelterHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/shelterhelper/ShelterHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package shelterhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelterHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
