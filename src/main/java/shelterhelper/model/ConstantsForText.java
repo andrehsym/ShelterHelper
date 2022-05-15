c5e667db15fca8fb87fdd15bbb24510b24717ea0
RickLyudmila <Lrick@yandex.ru>
2022-05-15 10:18:34 +0300

    v 1.0.1  first commit  by rickln.  Please, don't chanhe pom.xml!

---
+++ src/main/java/shelterhelper/model/ConstantsForText.java
@@ -0,0 +1,7 @@
+package shelterhelper.model;
+
+public class ConstantsForText {
+    public static final String STRING_FORMAT = "([0-9\\.\\:\\s]{16})(\\s)([\\W+]+)";
+    public static final String STRING_PATTERN = "dd.MM.yyyy HH:mm";
+
+}
