74ef1766a9ba09706269c7f7b589f27efff81dc1
RickLyudmila <Lrick@yandex.ru>
2022-06-17 22:08:48 +0300

    from rickln  17062022  - вопросы из справочника
    Feature 1

--- src/main/java/shelterhelper/model/Constants.java
+++ src/main/java/shelterhelper/model/Constants.java
@@ -12,5 +12,11 @@ public class Constants {
     public static final String EMOJI_DOG = (":dog:");
     public static final String EMOJI_CAR = (":truck:");
     public static final String EMOJI_HOUSE = (":house_with_garden:");
+    public static final String EMOJI_QUESTION = (":question:");
+    public static final String EMOJI_PERSON = (":man:");
+    public static final String EMOJI_PENCIL = (":pencil2:");
+
+
+
 
 }
