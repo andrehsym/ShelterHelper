636fe84c8c2acb5c3aecbbf532bdf847aea915f9
andrehsym <kochetkovandre1993@gmail.com>
2022-06-26 02:17:51 +0300

    26.06.: класс ContactsImpl метод callClientContacts() - исправлен regex, данные записи в БД, обработка ошибки класс UpdateListener - исправлен метод process(), startMethod(), checkingCall(), petReport() класс Constants - исправлено поле PHONE_PATTERN

--- src/main/java/shelterhelper/service/ContactsImpl.java
+++ src/main/java/shelterhelper/service/ContactsImpl.java
@@ -1,6 +1,8 @@
 package shelterhelper.service;
 
+import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
 import org.springframework.stereotype.Service;
 import shelterhelper.model.ShelterClient;
 import shelterhelper.repository.ShelterClientRepository;
@@ -14,32 +16,33 @@ import static shelterhelper.model.Constants.*;
 @Service
 public class ContactsImpl implements Contacts{
     private final ShelterClientRepository shelterClientRepository;
+    private final TelegramBot telegramBot;
 
-    public ContactsImpl(ShelterClientRepository shelterClientRepository) {
+    public ContactsImpl(ShelterClientRepository shelterClientRepository, TelegramBot telegramBot) {
         this.shelterClientRepository = shelterClientRepository;
+        this.telegramBot = telegramBot;
     }
 
     @Override
     public void callClientContacts(Update update) {
-        //        Pattern pattern = Pattern.compile("([\\W+]+)(\\s)(^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\\\.[A-Z]{2,6}$)(\\s)([\"^\\\\d{10}$\"]{12})"); //паттерн с именем клиента, которое он вводит - не ник в телеграме
+
         Pattern pattern = Pattern.compile(PHONE_PATTERN);
         Matcher matcher = pattern.matcher(update.message().text());
         try {
             matcher.matches();
-//            String name = matcher.group(1); //имя клиента, которое он вводит
             String email = matcher.group(1);
-            String phoneNumber = matcher.group(3);
+            String phoneNumber = matcher.group(4);
             LocalDateTime stamp = LocalDateTime.now();
             ShelterClient newbie = new ShelterClient();
             newbie.setIdChat(update.message().chat().id());
-            newbie.setNameUserInChat(update.chatMember().from().username());
+            newbie.setNameUserInChat(update.message().from().firstName()+" "+update.message().from().lastName()+" "+update.message().from().username());
             newbie.setStamp(stamp);
             newbie.setPhoneUser(phoneNumber);
             newbie.setEmailUser(email);
-            shelterClientRepository.save(newbie); //создать репозиторий ShelterClient
-//            telegramBot.execute(new SendMessage(update.message().chat().id(), "Ваши контакты для связи переданы"));
+            shelterClientRepository.save(newbie);
+            telegramBot.execute(new SendMessage(update.message().chat().id(), "Ваши контакты для связи переданы"));
         } catch (IllegalStateException e) {
-//            telegramBot.execute(new SendMessage(update.message().chat().id(), "Неправильный формат"));
+            telegramBot.execute(new SendMessage(update.message().chat().id(), "Неправильный формат контактов"));
         }
     }
 }
